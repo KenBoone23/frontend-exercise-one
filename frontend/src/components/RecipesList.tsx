4fc66c211a36efcfceecf1c11f53d27741a44d9d
Ken Boone <ken@digitalglow.be>
2021-11-16 22:43:11 +0100

    recipes list + pagination

--- frontend/src/components/RecipesList.tsx
+++ frontend/src/components/RecipesList.tsx
@@ -1,5 +1,73 @@
+import { Button, Box, Link } from "@chakra-ui/react";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Link as ReactRouterLink } from "react-router-dom";
+
 type RecipesListProps = {};
 
+type Recipe = {
+  name: string;
+  id: string;
+};
+
 export const RecipesList: React.FC<RecipesListProps> = ({}) => {
-  return <div>list view</div>;
+  const [page, setPage] = useState(1);
+  const fetchRecipes = (page = 1) =>
+    fetch(`http://localhost:4000/recipes/?page=${page}&size=3`).then((res) =>
+      res.json()
+    );
+
+  const {
+    isLoading,
+    isError,
+    error,
+    data,
+    isFetching,
+    isPreviousData,
+  } = useQuery<Recipe[], Error>(["recipes", page], () => fetchRecipes(page), {
+    keepPreviousData: true,
+  });
+
+  return (
+    <div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : isError ? (
+        <div>Error: {error?.message}</div>
+      ) : (
+        <Box maxW="lg" borderWidth="1px" borderRadius="lg" p="3" mb="5">
+          {data?.map((recipe) => (
+            <p key={recipe.id}>
+              <Link
+                as={ReactRouterLink}
+                color="teal.500"
+                to={`/recipes/${recipe.id}`}
+              >
+                {recipe.name}
+              </Link>
+            </p>
+          ))}
+        </Box>
+      )}
+      <Button
+        size="sm"
+        onClick={() => setPage((old) => Math.max(old - 1, 0))}
+        disabled={page === 1}
+      >
+        Prev
+      </Button>{" "}
+      <Button
+        size="sm"
+        onClick={() => {
+          if (!isPreviousData /*&& data?.hasMore*/) {
+            setPage((old) => old + 1);
+          }
+        }}
+        disabled={isPreviousData /*|| !data?.hasMore*/}
+      >
+        Next
+      </Button>
+      {isFetching ? <span> Loading...</span> : null}{" "}
+    </div>
+  );
 };
